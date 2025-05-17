582ed86bb50c06ab3569ece83ec85bee5648e27b
1kareaa <uraosetrov@mail.ru>
2025-05-17 12:34:33 +0300

    Update main.c
    Добавлена проверка возвращаемого значения и улучшена очистка буфера (вся строка до пробела

--- main.c
+++ main.c
@@ -5,13 +5,20 @@
 int main() 
 {
     int choice;
-    
+    int result;
     do
     {
         menu();
         printf("Vvesti nomer programy\n");
-        scanf("%d", &choice);
-        getchar(); //гечар тут забирает символ чтобы он не ушел в lr1, lr2 итд
+        result = scanf("%d", &choice);//проверка возвращаемого значения
+        while(getchar() != '\n'); // очистка буфера при неверном вводе
+        
+        if(result != 1) // если ввод не был успешно преобразован в число
+        {
+            printf("Oshibka vvoda. Vvedite chislo.\n");
+            continue; // пропускаем оставшуюся часть цикла и начинаем заново
+        }
+        
         switch(choice) 
         {
             case 1:
@@ -40,11 +47,6 @@ int main()
         }
     }
     while(choice != 0);
-    
-    return 0;
-}
-        }
-    } while(choice != 0);
     
     return 0;
 }
